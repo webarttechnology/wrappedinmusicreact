c92c10dd29b6200c5fecb696cdb7e51cb7f0c7b7
iamwebartdevteam <biswajit@webart.technology>
2023-06-14 17:03:29 +0530

    form validation and otp funcationalty done

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,8 @@
 import { useFormik } from "formik";
 import { userSchema } from "../schemas/Validation.js";
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
+import OTPInput from "react-otp-input";
 const initialValues = {
   name: "",
   email: "",
@@ -9,6 +10,8 @@ const initialValues = {
   confirmPassword: "",
 };
 const SignUp = () => {
+  const [isEmail, setIsEmail] = useState(0);
+  const [otp, setOtp] = useState("");
   const { values, errors, handleBlur, handleChange, handleSubmit, touched } =
     useFormik({
       initialValues: initialValues,
@@ -18,8 +21,11 @@ const SignUp = () => {
       },
     });
 
+  console.log("otp", otp);
+
   const submitButton = async (values) => {
     console.log("values", values);
+    setIsEmail(1);
   };
 
   return (
@@ -34,96 +40,120 @@ const SignUp = () => {
             />
           </div>
           <div class="ms_register_form">
-            <h2>Create an account</h2>
-            <form onSubmit={handleSubmit}>
-              <div class="form-group">
-                <input
-                  type="text"
-                  placeholder="Enter Your Name"
-                  class="form-control"
-                  name="name"
-                  value={values.name}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <span class="form_icon">
-                  <i class="fa_icon form-user" aria-hidden="true"></i>
+            {isEmail === 0 ? (
+              <>
+                <h2>Create an account</h2>
+                <form onSubmit={handleSubmit}>
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      placeholder="Enter Your Name"
+                      class="form-control"
+                      name="name"
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <span class="form_icon">
+                      <i class="fa_icon form-user" aria-hidden="true"></i>
+                    </span>
+                    {touched.name && errors.name ? (
+                      <>
+                        <p className="errorMess">{errors.name}</p>
+                      </>
+                    ) : null}
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      placeholder="Enter Your Email"
+                      class="form-control"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <span class="form_icon">
+                      <i class="fa_icon form-envelope" aria-hidden="true"></i>
+                    </span>
+                    {touched.email && errors.email ? (
+                      <>
+                        <p className="errorMess">{errors.email}</p>
+                      </>
+                    ) : null}
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="password"
+                      placeholder="Enter Password"
+                      class="form-control"
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <span class="form_icon">
+                      <i class="fa_icon form-lock" aria-hidden="true"></i>
+                    </span>
+                    {touched.password && errors.password ? (
+                      <>
+                        <p className="errorMess">{errors.password}</p>
+                      </>
+                    ) : null}
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="password"
+                      placeholder="Confirm Password"
+                      class="form-control"
+                      name="confirmPassword"
+                      value={values.confirmPassword}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <span class="form_icon">
+                      <i class=" fa_icon form-lock" aria-hidden="true"></i>
+                    </span>
+                    {touched.confirmPassword && errors.confirmPassword ? (
+                      <>
+                        <p className="errorMess">{errors.confirmPassword}</p>
+                      </>
+                    ) : null}
+                  </div>
+                  <button class="ms_btn">register now</button>
+                </form>
+                <p>
+                  Already Have An Account?{" "}
+                  <a
+                    href="#myModal1"
+                    data-toggle="modal"
+                    class="ms_modal hideCurrentModel"
+                  >
+                    login here
+                  </a>
+                </p>
+              </>
+            ) : (
+              <>
+                <h2 className="mb-4">Email verification</h2>
+                <span className="d-block">
+                  Enter the code we just send on your Email
                 </span>
-                {touched.name && errors.name ? (
-                  <>
-                    <p className="errorMess">{errors.name}</p>
-                  </>
-                ) : null}
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  placeholder="Enter Your Email"
-                  class="form-control"
-                  name="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <span class="form_icon">
-                  <i class="fa_icon form-envelope" aria-hidden="true"></i>
-                </span>
-                {touched.email && errors.email ? (
-                  <>
-                    <p className="errorMess">{errors.email}</p>
-                  </>
-                ) : null}
-              </div>
-              <div class="form-group">
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  class="form-control"
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <span class="form_icon">
-                  <i class="fa_icon form-lock" aria-hidden="true"></i>
-                </span>
-                {touched.password && errors.password ? (
-                  <>
-                    <p className="errorMess">{errors.password}</p>
-                  </>
-                ) : null}
-              </div>
-              <div class="form-group">
-                <input
-                  type="password"
-                  placeholder="Confirm Password"
-                  class="form-control"
-                  name="confirmPassword"
-                  value={values.confirmPassword}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <span class="form_icon">
-                  <i class=" fa_icon form-lock" aria-hidden="true"></i>
-                </span>
-                {touched.confirmPassword && errors.confirmPassword ? (
-                  <>
-                    <p className="errorMess">{errors.confirmPassword}</p>
-                  </>
-                ) : null}
-              </div>
-              <button class="ms_btn">register now</button>
-            </form>
-            <p>
-              Already Have An Account?{" "}
-              <a
-                href="#myModal1"
-                data-toggle="modal"
-                class="ms_modal hideCurrentModel"
-              >
-                login here
-              </a>
-            </p>
+                <div className="otpInput">
+                  <OTPInput
+                    value={otp}
+                    onChange={setOtp}
+                    numInputs={6}
+                    renderSeparator={<span className="dassh">-</span>}
+                    renderInput={(props) => <input {...props} />}
+                  />
+                </div>
+                <button class="ms_btn">Verify OTP</button>
+                <p>
+                  <span class="ms_modal resend">Resend OTP</span>
+                </p>
+              </>
+            )}
           </div>
         </div>
       </div>
